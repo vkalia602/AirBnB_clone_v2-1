cdb460281591763ebc11b79b99626a51517a10de
Lindsey Hemenez <lindseyhemenez@gmail.com>
2018-04-18 20:31:44 +0000

    Wrote script to configure servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# script sets up web servers for deployment of web_static folder
+my_string="\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n"
+sudo apt-get update
+sudo apt-get install nginx -y
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo touch /data/web_static/releases/test/index.html
+echo "Holberton school is cool" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data
+sed -i "41i\ $my_string" /etc/nginx/sites-available/default
+sudo service nignx restart
