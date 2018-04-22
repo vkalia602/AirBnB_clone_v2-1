96decdbc9b3289fc957e429d892fd70058de092f
Lindsey Hemenez <lindseyhemenez@gmail.com>
2018-04-22 02:13:46 +0000

    Adjusted method to specifically only look at State objects

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,7 +2,6 @@
 from models import storage
 from flask import Flask, render_template
 app = Flask(__name__)
-my_storage = storage.all()
 
 
 @app.teardown_appcontext
@@ -12,10 +11,10 @@ def close_db(error):
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
+    my_storage = storage.all('State')
     state_list = []
-    for key, vval in my_storage.items():
-        if 'State' in key:
-            state_list.append(my_storage[key])
+    for key, val in my_storage.items():
+        state_list.append(my_storage[key])
     return render_template('7-states_list.html', state_list=state_list)
 
 if __name__ == '__main__':
