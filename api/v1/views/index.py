7986175d9557b3eb17c489146f92fa23309513cb
vkalia602 <vkalia602@gmail.com>
2018-04-24 05:18:14 +0000

    Added route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,25 @@
 #!/usr/bin/python3
 from flask import Flask, jsonify
 from api.v1.views import app_views
-
+from models import storage
 @app_views.route('/status', strict_slashes=False)
 def status():
     return jsonify({"status": "OK"})
 
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    count_dict = {}
+    classes = {
+        "amenities": "Amenity",
+        "cities": "City",
+        "places": "Place",
+        "reviews": "Review",
+        "states": "State",
+        "users": "User"
+    }
+    for key, value in classes.items():
+        v = storage.count(value)
+        count_dict[key] = v
+    return jsonify(count_dict)
 if __name__ == '__main__':
     pass
