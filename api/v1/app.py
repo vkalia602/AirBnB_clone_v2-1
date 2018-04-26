4ecee5aa33488e334e48d09f405eb4b40b544939
vkalia602 <vkalia602@gmail.com>
2018-04-26 06:26:57 +0000

    added 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,8 +3,8 @@
 Module for flask web framework
 """
 
-from flask import Flask
 from models import storage
+from flask import Flask, jsonify, make_response
 from api.v1.views import app_views
 app = Flask(__name__)
 
@@ -13,6 +13,11 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def close_dat_sess(exception):
     storage.close()
+
+@app.errorhandler(404)
+def page_not_found(e):
+
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
