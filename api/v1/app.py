78f19738b549e0d98696829ee03dc59f1a6ea2b8
vkalia602 <vkalia602@gmail.com>
2018-04-27 04:26:05 +0000

    Trying things

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,11 +8,13 @@ from flask import Flask, jsonify, make_response
 from api.v1.views import app_views
 from flask_cors import CORS
 import os
-app = Flask(__name__)
 host = os.getenv('HBNB_API_HOST', default='0.0.0.0')
 port = int(os.getenv('HBNB_API_PORT', default='5000'))
+
+app = Flask(__name__)
 app.register_blueprint(app_views)
-CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+
 
 @app.teardown_appcontext
 def close_dat_sess(exception):
