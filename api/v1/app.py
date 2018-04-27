ddc19dbc234786dda2bb877edf579ff204738d5b
vkalia602 <vkalia602@gmail.com>
2018-04-27 00:46:15 +0000

    fixed connection port

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,9 +6,12 @@ Module for flask web framework
 from models import storage
 from flask import Flask, jsonify, make_response
 from api.v1.views import app_views
+import os
 app = Flask(__name__)
-
+host = os.getnev('HBNB_API_HOST', default='0.0.0.0')
+port = int(os.getnev('HBNB_API_PORT', default='5000'))
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def close_dat_sess(exception):
@@ -16,6 +19,7 @@ def close_dat_sess(exception):
     Method to close a session
     '''
     storage.close()
+
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -25,4 +29,4 @@ def page_not_found(e):
     return make_response(jsonify({"error": "Not found"}), 404)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host=host, port=port)
